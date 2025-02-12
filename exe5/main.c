191ac5e201706b4241924f6f2d9a66cb5ba56739
Rkzzin <henriquels5@al.insper.edu.br>
2025-02-12 19:14:26 -0300

    exe5:D

--- exe5/main.c
+++ exe5/main.c
@@ -21,25 +21,13 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
+            printf("Botao 1: %d\n", cnt_1++);
             sleep_ms(500);
-            if (!gpio_get(BTN_PIN)) {
-                cnt_1++;
-                printf("Botao 1: %d\n", cnt_1);
-                while (!gpio_get(BTN_PIN)) {
-
-                }
-            }
         }
 
         if (!gpio_get(BTN_PIN_2)) {
+            printf("Botao 2: %d\n", cnt_2++);
             sleep_ms(500);
-            if (!gpio_get(BTN_PIN_2)) {
-                cnt_2++;
-                printf("Botao 2: %d\n", cnt_2);
-                while (!gpio_get(BTN_PIN_2)) {
-
-                }
-            }
         }
     }
 }
